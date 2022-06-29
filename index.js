718881e25df56a059ee6b528808046520acb044f
Michael Dear <michaelcdear@gmail.com>
2022-06-29 11:06:15 -0600

    commit tree a3dc89045561cb4353c71a6a62b99bd9a8466041

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function superbowlWin(array) {
+    let winYearElement = array.find(findWin)
+    if (winYearElement) {return winYearElement.year} else {
+        return undefined
+    }
+}
+
+function findWin(element, index, array) {
+   if (element.result === "W") {return (element.year)}
+}
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+  superbowlWin(record)
+
+// element, index, array
